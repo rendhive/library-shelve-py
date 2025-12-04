eeb07a88ce423d28cf899cb5a2ee6802dee58bb0
rendhive <rendhivecode@gmail.com>
2025-12-05 01:39:58 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul shelve.py

---
+++ 1. Mengimpor Modul shelve.py
@@ -0,0 +1,5 @@
+import shelve
+
+print("Modul shelve berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk penyimpanan objek Python berbasis key/value.
+# Kondisi: Digunakan sebelum memanfaatkan fungsionalitas dari shelve.
